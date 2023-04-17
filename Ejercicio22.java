17bc9f4c954bad8a1ea7e52196420ea5d351f6eb
Alejandro Castro <111087081+AlejandroC10P@users.noreply.github.com>
2023-04-17 17:03:24 -0500

    Add files via upload

---
+++ Ejercicio22.java
@@ -0,0 +1,8 @@
+package Capitulo_4;
+
+public class Ejercicio22 {
+    public static void main(String[] args) {
+        InterfazEjercicio22 form = new InterfazEjercicio22();
+        form.setVisible(true);
+    }
+}
---
+++ SalarioEjercicio22.java
@@ -0,0 +1,9 @@
+package Capitulo_4;
+
+public class SalarioEjercicio22 {
+    public static double calcularSalarioMensual(double salarioHora, double horasTrabajadasMes){
+        double salarioMensual;
+        salarioMensual = salarioHora * horasTrabajadasMes;
+        return salarioMensual;
+    }
+}
